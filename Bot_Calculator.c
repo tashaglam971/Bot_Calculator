466cc83a81a6b8d643ff9717d257189013576f93
war_goddess300 <154751143+tashaglam971@users.noreply.github.com>
2025-07-20 20:57:31 -0400

    Update Bot_Calculator.c

--- Bot_Calculator.c
+++ Bot_Calculator.c
@@ -15,14 +15,14 @@ int main() {
 //Calculate command
 int prix;
 
-char hacker_input[50];
+char user_input[50];
 char commands[10] = "Calculate";
 char add_command[10] = "add";
 
 
 printf("Cindy GLAM, entrez une commande: ");
-fgets(hacker_input, sizeof(hacker_input), stdin);
-hacker_input[strcspn(hacker_input, "\n")] = 0;
+fgets(user_input, sizeof(user_input), stdin);
+hacker_input[strcspn(user_input, "\n")] = 0;
 
 if (strcmp(hacker_input, commands) == 0) {
 printf("Entrez les digits que vous voulez calculer: ");
